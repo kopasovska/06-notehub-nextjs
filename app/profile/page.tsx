d4e5c4917d5996d6883140d5c485587db9adedf2
kopasovska <v.kopasovska@gmail.com>
2025-12-01 07:39:25 +0100

    Initial commit

---
+++ app/profile/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'My profile',
+};
+
+export default function Profile() {
+  return <div>Profile</div>;
+}
